cf28071a6789c4869272a7bff0d5512cb9dc4633
kidd1492 <chris.wr1492@gmail.com>
2025-03-13 14:33:52 -0400

    updated main to call analyzers

--- main.py
+++ main.py
@@ -4,23 +4,10 @@ import os, re
 
 def main():
     directory = get_directory()
+    gather_file_types(directory)
+    call_python_analyzer(directory)
     
-    analyze = PythonProjectAnalyzer(directory)
-    analyze.analize()
-    analyze.full_report()
-    #python_files = analyze.python_files
-    #print(python_files)
-
-    '''
-    analyzer = BaseAnalyzer(directory)
-    analyzer.gather_files()
-    files = analyzer.files
-    print(files)
-    file_type_list = analyzer.file_type_list
-    print(file_type_list)
-    '''
-
-
+   
 def get_directory():
     ''' ask user for filepath '''
     path_pattern = r"^[A-Z]:[\\/](?:[^\\/]+[\\/])*[^\\/]*$"
@@ -33,6 +20,23 @@ def get_directory():
                     return directory_name
             except Exception as e:
                 print(f"An error occurred: {e}. Please try again.")      
+
+
+#Uses BaseAnalyzer to get file types and decied what analizer to run
+def gather_file_types(directory):
+    analyzer = BaseAnalyzer(directory)
+    analyzer.gather_files()
+    file_type_list = analyzer.file_type_list
+    print(file_type_list)
+
+
+#functions to call the analyzers 
+def call_python_analyzer(directory):
+    python_analyzer = PythonProjectAnalyzer(directory)
+    python_analyzer.analize()
+    python_analyzer.full_report()
+
+
 
 
 if __name__ == "__main__":
