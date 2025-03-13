135fca25da5710308c0ed72851af12e419c7e77c
kidd1492 <chris.wr1492@gmail.com>
2025-03-13 17:29:16 -0400

    html files gathered in html analyzer

--- main.py
+++ main.py
@@ -31,20 +31,23 @@ def gather_file_types(directory):
     if "py" in file_type_list:
         call_python_analyzer(directory)
 
-    if "md" in file_type_list:
+    if "html" in file_type_list:
         call_html_analyzer(directory)
 
 
 #functions to call the analyzers 
 def call_python_analyzer(directory):
+    ...
     python_analyzer = PythonProjectAnalyzer(directory)
-    python_analyzer.analyze() #fix this
+    python_analyzer.analyze()
     python_analyzer.full_report()
 
 
 def call_html_analyzer(directory):
     html_analyzer = HTMLAnalyzer(directory)
     html_analyzer.analyze()
+    html_files = html_analyzer.html_files
+    print(html_files)
 
 
 
