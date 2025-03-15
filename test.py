0a524ec20ff8e67576296b2f53e2c7c95eff3bf9
kidd1492 <chris.wr1492@gmail.com>
2025-03-15 13:47:28 -0400

    processing data in test??

--- test.py
+++ test.py
@@ -1,32 +1,80 @@
 import csv
 
+# Load data into a list of dictionaries
+with open("python_summary.csv", mode='r') as file:
+    data = list(csv.DictReader(file))
+
+
+#TODO change main function to test call it from main.py
 def main():
-   print()
-   python_files = gather_python_files()
-   for file in python_files: print(f"{file}")
-   print()
-   comments()
-
-
-def gather_python_files():
+    imports()
+    p_files()
+    classes()
+    functions() 
+    comments()
+ 
+    
+def p_files():
     python_files = []
-    with open("python_summary.csv") as file:
-        df = csv.DictReader(file)
-        #collect file names in python_files. 
-        for row in df:
-            if row["File"] not in python_files:
-                python_files.append(row["File"])
-    return python_files
+    # Collect file names in python_files
+    for row in data:
+        if row["File"] not in python_files:
+            python_files.append(row["File"])
+    print(f"File List:\n")
+    print(f"{python_files}\n")
+
+
+def imports():
+    imports_list = []
+    # Collect all imports in a list
+    for row in data:
+        if row["Type"] == "import":
+            row = row["Content"].split()
+            if row[0] == "import":
+                if row[1] not in imports_list: 
+                    if "," in row[1]:
+                        one = row[1].split(",")
+                        for i in one:
+                            if i not in imports_list:
+                                imports_list.append(i)
+                else:
+                    imports_list.append(row[1:])                   
+
+            if row[0] == "from":
+                if row[1].startswith("."):
+                    continue
+                else:
+                    imports_list.append(row[1]) 
+    print(f"Imports: \n")
+    for item in imports_list: print(item)
+    print()
+
+
+def classes():
+    print(f"Class: \n")
+    for row in data:
+        if row["Type"] == "class":
+            print(f"{row['Content']}")
+    print()
+
+
+def functions():
+    print(f"Functions: \n")
+    for row in data:
+        if row["Type"] == "function":
+            print(row["Content"])
+    print()
 
 
 def comments():
-    with open("python_summary.csv") as file:
-        df = csv.DictReader(file)
-        #collect file names in python_files. 
-        for row in df:
-            if row["Type"] == "comment":
-                print (row["Content"])
+    # Collect all comments from all files
+    print(f"Comments: \n")
+    for row in data:
+        if row["Type"] == "comment":
+            print(row["Content"])
     print()
+
+
 
 
 
