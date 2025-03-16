8339adb178bdcb8fb8b56abd7fdcc64c87d869c6
kidd1492 <chris.wr1492@gmail.com>
2025-03-15 21:35:12 -0400

    set up base to print dif python reports.

--- analyzers/base_analyzer.py
+++ analyzers/base_analyzer.py
@@ -8,6 +8,7 @@ class BaseAnalyzer:
         self.file_type_list = file_type_list
         self.file_count = 0
 
+
     def count_files(self):
         for file in self.all_file_paths: self.file_count += 1
 
@@ -24,5 +25,16 @@ class BaseAnalyzer:
                 break
             
         if report_type == "py":
-            print(f"Python")
-            test.main()
+            while True:
+                print(f"\n Python \n")
+                print("1. Full Report\n2. File List\n3. Unique Imports\n 4.Classes\n5.Functions\n6. Comments\n")
+                function_number = input("Enter Number for Report or exit: ")
+                if function_number != "exit":
+                    if function_number == "1": test.main()
+                    if function_number == "2": test.p_files()
+                    if function_number == "3": test.imports()
+                    if function_number == "4": test.classes()
+                    if function_number == "5": test.functions()
+                    if function_number == "6": test.comments()
+                else:
+                    break
