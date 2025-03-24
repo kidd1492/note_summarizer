45a2475bcbbbc224a3bab406d050f9c06ad11263
kidd1492 <chris.wr1492@gmail.com>
2025-03-23 20:12:55 -0400

    added the all files all data

--- reports/report_helper.py
+++ reports/report_helper.py
@@ -31,29 +31,45 @@ def select_file(df, selected_file_type):
     while True:
         print("\n")
         for item in file_list:print(item)
-        print("\n","Type file name or all for all files")
+        print("\n","Type file name or [all] for all files")
         selected_file = input("or [back] to go back to File Types: ")
         if selected_file == "back":file_type()
-        elif selected_file in file_list:select_data(df, selected_file, selected_file_type, type)
+        elif selected_file in file_list or selected_file == "all":
+            select_data(df, selected_file, selected_file_type, type)
 
 
 def select_data(df, selected_file, selected_file_type, type):
-    data_type = type[type['FileName'] == selected_file]
-
-    data_list = data_type['Type'].unique()
-
-    while True:
-        print("\n")
-        for item in data_list:print(item)
-        print(f"\n [back] to go back to file selection:")
-        selected_data = input("Select Data Type: ")
-        if selected_data == "back":select_file(df, selected_file_type)
-
-        elif selected_data in data_list:
-            filtered_df = data_type[data_type['Type'] == f"{selected_data}"]
+    if selected_file != "all":
+        data_type = type[type['FileName'] == selected_file]
+        data_list = data_type['Type'].unique()
+        while True:
             print("\n")
-            print(filtered_df)
+            for item in data_list:print(item)
+            print(f"\n [back] to go back to file selection:")
+            selected_data = input("Select Data Type: ")
+            if selected_data == "back":select_file(df, selected_file_type)
+            if selected_data == "all":
+                print("\n")
+                print(data_type)
+            elif selected_data in data_list:
+                filtered_df = data_type[data_type['Type'] == f"{selected_data}"]
+                print("\n")
+                print(filtered_df)
 
+    else:
+        data_list = type['Type'].unique()
+        while True:
+            
+            print("\n")
+            for item in data_list:print(item)
+            print(f"\n [back] to go back to file selection:")
+            selected_data = input("Select Data Type: ")
+            if selected_data == "back":select_file(df, selected_file_type)
+            elif selected_data in data_list:
+                filtered_df = type[type['Type'] == f"{selected_data}"]
+                print("\n")
+                print(filtered_df)
+        
 
 def analyzed_file_summary(categorized_files):
     total_analyzed_files = 0
